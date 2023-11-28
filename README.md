45ad963cd7b28425c72d0aa7c23205ee28df76e5
anbuleo <anbuliyon@gmail.com>
2023-11-28 21:22:20 +0530

    create pages and routes

--- README.md
+++ README.md
@@ -50,7 +50,28 @@ __npm run dev__
 
 
 **gitcommand**
+…or create a new repository on the command line
+echo "# mern_realEstate" >> README.md
+  git init
+  git add README.md
+  git commit -m "first commit"
+  git branch -M main
+  git remote add origin https://github.com/anbuleo/mern_realEstate.git
+  git push -u origin main
+
+  …or push an existing repository from the command line
+git remote add origin https://github.com/anbuleo/mern_realEstate.git
+  git branch -M main
+  git push -u origin main
+
+
  
  git init => to inizilize the repostariy
  git add . => its to add all
+ git commit -m "message" => to add a message for this commit
+ git remote add origin https://github.com/anbuleo/mern_realEstate.git
+
+ npm i react-router-dom
+
+ to add routing concepts
  
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import Signup from './pages/SignUp'
+import Profile from './pages/Profile'
+import About from './pages/About'
 
 function App() {
-  return  <h1 className='text-rose-500'>App</h1>
-  
+  return  <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/sign-in' element={<SignIn />} />
+              <Route path='/sign-up' element={<Signup />} />
+              <Route path='/profile' element={<Profile />} />
+              <Route path='/about' element={<About />} />
+              
+            </Routes>
+          </BrowserRouter>
 }
 
 export default App
