2743f027d624c7ca63f371c64792e725229ac2b6
anbuleo <anbuliyon@gmail.com>
2023-12-01 12:58:28 +0530

    added redux-persist

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from './App.jsx'
 import './index.css'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { store } from './redux/store.js';
+import { persistor, store } from './redux/store.js';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor} >
     <App />
+    </PersistGate>
     <ToastContainer
     autoClose={1000}/>
   </Provider>,
